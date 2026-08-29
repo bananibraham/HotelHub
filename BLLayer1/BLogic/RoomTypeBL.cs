3167cf4ef6748cf8bb63b9bcd4dd43b9c386cdfb
tasneem <tasneemelzend985@gmail.com>
2026-08-29 23:27:30 +0300

    Add RoomType CRUD

---
+++ BLLayer1/BLogic/RoomTypeBL.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLLayer1.BLogic
+{
+    internal class RoomTypeBL
+    {
+    }
+}
---
+++ BLLayer1/Interfaces/IRoomTypeBL.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLLayer1.Interfaces
+{
+    internal interface Interface1
+    {
+    }
+}
